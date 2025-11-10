45f2c461f03fc0928ed6c78852a25e5ce081439d
Amarmaity <Amarmaity243@gmail.com>
2025-11-10 18:28:07 +0530

    Delete evaluation directory

--- evaluation/resources/views/delostyleUsers/hr-dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app') <!-- Extends app.blade.php (Header, Sidebar, Footer included) -->
-
-@section('title', 'HR Dashboard') <!-- Page Title -->
-
-@section('breadcrumb', 'HR') <!-- Breadcrumb -->
-
-@section('page-title', 'HR Dashboard') <!-- Page Title in Breadcrumb -->
-
